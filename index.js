e3698f2cb1cfd6875c1b5a68603c7862fcae863e
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-09-26 19:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,12 +27,10 @@ function dec(n) {
 describe('makeInt(n)', 
 function makeInt(n) {
   parseInt(a)
-  }
-describe('makeInt(n)',
+  })
 function makeInt(n) {
     parseInt('0x2328', 10)
   }
-
   it('returns NaN as appropriate', function() {
     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
   })
